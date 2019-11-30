716b7eae251b03e2d7bdd6ce7398d71c1419c258
HermanLin <hlin9@stuy.edu>
2019-11-30 00:55:45 -0500

    finished constructor and output operator

--- homework/hw08/polynomial.cpp
+++ homework/hw08/polynomial.cpp
@@ -12,12 +12,10 @@ struct Node {
 class Polynomial {
     friend ostream& operator<<(ostream&, const Polynomial);
 private:
-    Node* coefficient;
+    Node* coeffPtr;
     int degree;
 public:
     Polynomial (vector<int>);
-
-    
 };
 
 // void doNothing(Polynomial temp) {}
@@ -31,11 +29,11 @@ int main() {
     Polynomial p4({5, 4, 3, 2, 1});      // 5x^4 + 4x^3 + 3x^2 + 2x + 1
     Polynomial has_a_zero({4, 0, 1, 7}); // 4x^3 + x + 7
 	
-    // cout << "p1: " << p1 << endl;
-    // cout << "p2: " << p2 << endl;
-    // cout << "p3: " << p3 << endl;
-    // cout << "p4: " << p4 << endl;
-    // cout << "has_a_zero: " << has_a_zero << endl;
+    cout << "p1: " << p1 << endl;
+    cout << "p2: " << p2 << endl;
+    cout << "p3: " << p3 << endl;
+    cout << "p4: " << p4 << endl;
+    cout << "has_a_zero: " << has_a_zero << endl;
     // cout << "p2 + p3: " << (p2+p3) << endl; 
     // cout << "p2 + p4: " << (p2+p4) << endl; 
     // cout << "p4 + p2: " << (p4+p2) << endl;
@@ -97,18 +95,41 @@ int main() {
 /* ======================== */
 
 Polynomial::Polynomial (vector<int> coeffs) : degree(coeffs.size() - 1) {
-    coefficient = new Node(coeffs[0]);
-    Node* ptr = coefficient;
-    for (size_t i = coeffs.size() - 1; i > 1; --i) {
-        ptr->next = new Node(coeffs[i]);
+    int index = coeffs.size() - 1; // last index
+    coeffPtr = new Node(coeffs[index]); // first node is last coeff
+    Node* ptr = coeffPtr;
+    // loop through coeffs backwards, adding nodes to the linked list
+    while (index) {
+        --index;
+        ptr->next = new Node(coeffs[index]);
         ptr = ptr->next;
     }
 }
 
 ostream& operator<<(ostream& os, const Polynomial poly) {
-    Node* ptr = poly.coefficient;
+    // use an temp array to print
+    int index = 0;
+    int prntArr [poly.degree + 1];
+    Node* ptr = poly.coeffPtr;
+    // fill array in order of linked list
     while (ptr) {
-
+        prntArr[index] = ptr->data;
+        ptr = ptr->next;
+        ++index;
     }
+    // loop through array backwards to print, using index as the 'power'
+    for (int i = poly.degree; i > 0; --i) { 
+        // if the coefficient is 0, don't print
+        if (prntArr[i] == 0) continue;
+        // elseif the coefficient is 1, only print 'x'
+        else if (prntArr[i] == 1) os << 'x';
+        else os << prntArr[i] << 'x';
+
+        // if the degree is 1, print " + "
+        if (i == 1) os << " + ";
+        else os << '^' << i << " + ";
+    }
+    // print constant
+    os << prntArr[0];
     return os;
 }
